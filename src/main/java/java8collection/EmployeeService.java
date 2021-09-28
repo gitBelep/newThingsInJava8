6f7f6e7aa398b33efb37c21a42f9f4faf603f3a8
gitBelep <ddawid3@freemail.hu>
2021-09-28 10:57:47 +0200

    Initial commit

---
+++ src/main/java/java8collection/EmployeeService.java
@@ -0,0 +1,42 @@
+package java8collection;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EmployeeService {
+
+    public List<Employee> removeWhereSalaryIsLowerThan(List<Employee> employees, int maxSalary) {
+        return employees;
+    }
+
+    public List<String> trimmedNames(List<String> names) {
+        return names;
+    }
+
+    public List<Employee> sortByName(List<Employee> employees) {
+        return employees;
+    }
+
+    public List<Employee> convertNamesToLowerCase(List<Employee> employees) {
+        return employees;
+    }
+
+//    public Map<String, Employee> firstEmployeeWithFirstName(List<Employee> employees) {
+//        return firstEmployees;
+//    }
+
+//    public Map<String, Integer> countByFirstName(List<Employee> employees) {
+//        return counts;
+//    }
+
+    public Map<Long, Employee> updateEmployees(Map<Long, Employee> base, List<Employee> changedEmployees) {
+        return base;
+    }
+
+    public Map<Long, Integer> salariesChanged(Map<Long, Integer> base, List<Employee> changedEmployees) {
+        return base;
+    }
+
+}
