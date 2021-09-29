7fcccff410c01939f03003f28536c22926a6a33c
gitBelep <ddawid3@freemail.hu>
2021-09-29 08:21:10 +0200

    Collections

--- src/main/java/java8collection/EmployeeService.java
+++ src/main/java/java8collection/EmployeeService.java
@@ -1,42 +1,77 @@
 package java8collection;
 
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class EmployeeService {
 
     public List<Employee> removeWhereSalaryIsLowerThan(List<Employee> employees, int maxSalary) {
-        return employees;
+        return employees.stream()
+                .filter(e -> e.getSalary() > maxSalary)
+                .collect(Collectors.toList());
+//        or: employees.removeIf(e -> e.getSalary() < maxSalary);
     }
 
     public List<String> trimmedNames(List<String> names) {
-        return names;
+        return names.stream()
+                .map(name -> name.trim())
+                .collect(Collectors.toList());
+//        or: names.replaceAll(String::trim);
     }
 
     public List<Employee> sortByName(List<Employee> employees) {
-        return employees;
+        return employees.stream()
+                .sorted( (e1, e2) -> e1.getName().compareTo(e2.getName()))
+                .collect(Collectors.toList());
+//        or: employees.sort(Comparator.comparing(Employee::getName));
     }
 
     public List<Employee> convertNamesToLowerCase(List<Employee> employees) {
-        return employees;
+        return employees.stream()
+                .peek(e -> e.setName(e.getName().toLowerCase()))
+                .collect(Collectors.toList());
+//        or: employees.forEach(e -> e.setName(e.getName().toLowerCase()));
     }
 
-//    public Map<String, Employee> firstEmployeeWithFirstName(List<Employee> employees) {
-//        return firstEmployees;
-//    }
+    public Map<String, Integer> countByFirstName(List<Employee> employees) {
+        Map<String, Integer> counts = new HashMap<>();
+        for (Employee e : employees){
+            String firstName = e.getName().substring(0, e.getName().indexOf(" "));
+            counts.merge(firstName, 1, (k,v) -> v + 1);
+        }
+        return counts;
+    }
 
-//    public Map<String, Integer> countByFirstName(List<Employee> employees) {
-//        return counts;
-//    }
+    public Map<String, Employee> firstEmployeeWithFirstName(List<Employee> employees) {
+        Map<String, Employee> firstEmployees = new HashMap<>();
+        for (Employee e : employees){
+            String firstName = e.getName().substring(0, e.getName().indexOf(" "));
+//     or:  String firstName = employee.getName().split(" ")[0];
+            firstEmployees.putIfAbsent(firstName, e);
+        }
+        return firstEmployees;
+    }
 
     public Map<Long, Employee> updateEmployees(Map<Long, Employee> base, List<Employee> changedEmployees) {
+        for(Employee e : changedEmployees){
+            base.replace(e.getId(), e);
+        }
         return base;
     }
 
     public Map<Long, Integer> salariesChanged(Map<Long, Integer> base, List<Employee> changedEmployees) {
+        for (Employee e : changedEmployees){
+            base.remove(e.getId(), e.getSalary());      //remove if did not change
+            base.replace(e.getId(), e.getSalary());     //replace if still exist
+        }
         return base;
-    }
+    }     // OR:
+//            if( base.get( e.getId() ) ==  e.getSalary()){
+//                base.remove(e.getId());
+//            } else {
+//                base.put(e.getId(), e.getSalary());
+//            }
 
 }
--- src/test/java/java8collection/EmployeeServiceTest.java
+++ src/test/java/java8collection/EmployeeServiceTest.java
@@ -3,8 +3,7 @@ package java8collection;
 import org.junit.Test;
 import java.util.*;
 import java.util.stream.Collectors;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class EmployeeServiceTest {
 
@@ -21,69 +20,80 @@ public class EmployeeServiceTest {
         List<Employee> removed = employeeService.removeWhereSalaryIsLowerThan(new ArrayList<>(employees),
                 190_000);
         assertEquals(Arrays.asList("Jane Doe"), removed.stream().map(Employee::getName).collect(Collectors.toList()));
+
+        List<Employee> removed2 = employeeService.removeWhereSalaryIsLowerThan(new ArrayList<>(employees),
+                170_000);
+        List<String> remainingNames = removed2.stream().map(Employee::getName).collect(Collectors.toList());
+        assertTrue( remainingNames.contains("John Doe"));
+        assertEquals(2, remainingNames.size());
     }
 
-//    @Test
-//    public void test_trimmedNames() {
-//        List<String> names = Arrays.asList("    John Doe    ", "   Jane Doe    ");
-//        List<String> replaced = employeeService.trimmedNames(names);
-//        assertEquals(Arrays.asList("John Doe", "Jane Doe"), replaced);
-//    }
-//
-//    @Test
-//    public void test_sortByName() {
-//        List<Employee> sorted = employeeService.sortByName(new ArrayList<>(employees));
-//        assertEquals(Arrays.asList("Jane Doe", "Joe Doe", "John Doe", "John Smith"),
-//                sorted.stream().map(Employee::getName).collect(Collectors.toList()));
-//    }
-//
-//    @Test
-//    public void test_convertNamesToLowerCase() {
-//        List<Employee> converted = employeeService.convertNamesToLowerCase(new ArrayList<>(employees));
-//        assertEquals(Arrays.asList("john doe", "jane doe", "joe doe", "john smith"),
-//                converted.stream().map(Employee::getName).collect(Collectors.toList()));
-//    }
-//
-//    @Test
-//    public void test_countByFirstName() {
-//        Map<String, Integer> counts = employeeService.countByFirstName(new ArrayList<>(employees));
-//        assertEquals(Integer.valueOf(2), counts.get("John"));
-//        assertEquals(Integer.valueOf(1), counts.get("Jane"));
-//    }
-//
-//    @Test
-//    public void test_firstEmployeeWithFirstName() {
-//        Map<String, Employee> names = employeeService.firstEmployeeWithFirstName(employees);
-//        assertEquals(3, names.size());
-//        assertEquals("John Doe", names.get("John").getName());
-//        assertEquals("Jane Doe", names.get("Jane").getName());
-//    }
-//
-//    @Test
-//    public void test_updateEmployees() {
-//        List<Employee> changedEmployees = Arrays.asList(new Employee(1L, "John Doe", 250_000),
-//                new Employee(5L,"Jane Smith", 100_000));
-//
-//        Map<Long, Employee> updated = employeeService.updateEmployees(new HashMap<>(employees.stream().collect(Collectors.toMap(Employee::getId, e -> e))),
-//                changedEmployees);
-//
-//        assertEquals(4, updated.size());
-//        assertEquals(250_000, updated.get(1L).getSalary());
-//    }
-//
-//    @Test
-//    public void test_salariesChanged() {
-//        List<Employee> changedEmployees = Arrays.asList(new Employee(1L, "John Doe", 180_001),
-//                new Employee(2L, "Jane Doe", 200_001),
-//                new Employee(3L,"Joe Doe", 100_000),
-//                new Employee(4L, "John Smith", 100_001));
-//
-//        Map<Long, Integer> changed = employeeService.salariesChanged(
-//                employees.stream().collect(Collectors.toMap(Employee::getId, Employee::getSalary)),
-//                changedEmployees
-//        );
-//
-//        assertEquals(3, changed.size());
-//        assertFalse(changed.containsKey(3L));
-//    }
+    @Test
+    public void test_trimmedNames() {
+        List<String> names = Arrays.asList("    John Doe    ", "   Jane Doe    ");
+        List<String> replaced = employeeService.trimmedNames(names);
+        assertEquals(Arrays.asList("John Doe", "Jane Doe"), replaced);
+    }
+
+    @Test
+    public void test_sortByName() {
+        List<Employee> sorted = employeeService.sortByName(new ArrayList<>(employees));
+        assertEquals(Arrays.asList("Jane Doe", "Joe Doe", "John Doe", "John Smith"),
+                sorted.stream().map(Employee::getName).collect(Collectors.toList()));
+    }
+
+    @Test
+    public void test_convertNamesToLowerCase() {
+        List<Employee> converted = employeeService.convertNamesToLowerCase(new ArrayList<>(employees));
+        assertEquals(Arrays.asList("john doe", "jane doe", "joe doe", "john smith"),
+                converted.stream().map(Employee::getName).collect(Collectors.toList()));
+    }
+
+    @Test
+    public void test_countByFirstName() {
+        Map<String, Integer> counts = employeeService.countByFirstName(new ArrayList<>(employees));
+        assertEquals(Integer.valueOf(2), counts.get("John"));
+        assertEquals(Integer.valueOf(1), counts.get("Jane"));
+    }
+
+    @Test
+    public void test_firstEmployeeWithFirstName() {
+        Map<String, Employee> names = employeeService.firstEmployeeWithFirstName(employees);
+        assertEquals(3, names.size());
+        assertEquals("John Doe", names.get("John").getName());
+        assertEquals("Jane Doe", names.get("Jane").getName());
+    }
+
+    @Test
+    public void test_updateEmployees() {
+        List<Employee> changedEmployees = Arrays.asList(
+                new Employee(1L, "John Doe", 250_000),
+                new Employee(5L,"Jane Smith", 100_000) );
+
+        Map<Long, Employee> updated = employeeService.updateEmployees(
+                new HashMap<>(                                          //param1: Map
+                employees.stream()
+                        .collect(Collectors.toMap(Employee::getId, e -> e))),
+                changedEmployees );                                     //param2: List
+
+        assertEquals(4, updated.size());
+        assertEquals(250_000, updated.get(1L).getSalary());
+    }
+
+    @Test
+    public void test_salariesChanged() {
+        List<Employee> changedEmployees = Arrays.asList(
+                new Employee(1L, "John Doe", 180_001),
+                new Employee(2L, "Jane Doe", 200_001),
+                new Employee(3L,"Joe Doe", 100_000),
+                new Employee(4L, "John Smith", 100_001));
+
+        Map<Long, Integer> changed = employeeService.salariesChanged(
+                employees.stream().collect(Collectors.toMap(Employee::getId, Employee::getSalary)),
+                changedEmployees );
+
+        assertEquals(3, changed.size());
+        assertFalse(changed.containsKey(3L));
+    }
+
 }
